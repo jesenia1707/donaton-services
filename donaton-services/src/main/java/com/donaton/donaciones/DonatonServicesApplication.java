c2c0fe13e9e43afb3321c2ce3533ae45a82a23cc
jesenia1707 <jes.pardo@duocuc.cl>
2026-04-25 18:17:55 -0400

    primer commit

---
+++ donaton-services/src/main/java/com/donaton/donaciones/DonatonServicesApplication.java
@@ -0,0 +1,13 @@
+package com.donaton.donaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonatonServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonatonServicesApplication.class, args);
+	}
+
+}

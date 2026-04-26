4c317a19db73cc8a5677440c3aa6cf200fd32785
jesenia1707 <jes.pardo@duocuc.cl>
2026-04-26 19:23:00 -0400

    defino nombre de tabla y actualizo ruta del endpoint"

--- donaton-services/src/main/java/com/donaton/donaciones/controller/DonacionController.java
+++ donaton-services/src/main/java/com/donaton/donaciones/controller/DonacionController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/donaciones")
+@RequestMapping("/api/donaciones")
 @CrossOrigin("*")
 public class DonacionController {
 
--- donaton-services/src/main/java/com/donaton/donaciones/entity/Donacion.java
+++ donaton-services/src/main/java/com/donaton/donaciones/entity/Donacion.java
@@ -7,6 +7,7 @@ import lombok.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "donaciones")
 public class Donacion {
 
     @Id
